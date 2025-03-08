b69432c9a40fce1471a7294fa27a547efbc41d90
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-03-07 22:56:37 -0500

    Node Get Started

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const http = require("http")
-
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" })
-    res.end("Hello World!")
-  })
-  .listen(8080)
+const { createServer } = require("node:http")
+const hostname = "127.0.0.1"
+const port = 3000
+const server = createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader("Content-Type", "text/plain")
+  res.end("Hello World")
+})
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
