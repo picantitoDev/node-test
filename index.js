6fb51e74baa6d7b30677df724863fbc84c392ed0
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-03-09 23:41:58 -0500

    Get Post requests

--- index.js
+++ index.js
@@ -10,21 +10,82 @@
 //   console.log(`Server running at http://${hostname}:${port}/`)
 // })
 
-const os = require("os")
+// const os = require("os")
 
-var totalMemory = os.totalmem()
-var freeMemory = os.freemem()
+// var totalMemory = os.totalmem()
+// var freeMemory = os.freemem()
 
-console.log(`Total Memory: ${totalMemory}, Free Memory: ${freeMemory}`)
+// console.log(`Total Memory: ${totalMemory}, Free Memory: ${freeMemory}`)
 
-const fs = require("fs")
-const files = fs.readdirSync("./")
-console.log(files)
+// const fs = require("fs")
+// const files = fs.readdirSync("./")
+// console.log(files)
 
-fs.readdir("$", function (err, files) {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log(files)
-  }
+// fs.readdir("$", function (err, files) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(files)
+//   }
+// })
+
+// import EventEmitter from "events"
+
+// const emitter = new EventEmitter()
+
+// emitter.emit("messageLogged")
+
+// import { request } from "https"
+
+// const options = {
+//   hostname: "example.com",
+//   port: 443,
+//   path: "/todos",
+//   method: "GET",
+// }
+
+// const req = request(options, (res) => {
+//   console.log(`statusCode: ${res.statusCode}`)
+
+//   res.on("data", (d) => {
+//     process.stdout.write(d)
+//   })
+// })
+
+// req.on("error", (error) => {
+//   console.error(error)
+// })
+
+// req.end()
+
+const https = require("https")
+
+const data = JSON.stringify({
+  todo: "Buy the milk",
 })
+
+const options = {
+  hostname: "whatever.com",
+  port: 443,
+  path: "/todos",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Content-Length": data.length,
+  },
+}
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on("data", (d) => {
+    process.stdout.write(d)
+  })
+})
+
+req.on("error", (error) => {
+  console.error(error)
+})
+
+req.write(data)
+req.end()
