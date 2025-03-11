7de78886f791e1df7aa6e9775d6c60bf721bd9f8
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-03-10 22:00:54 -0500

    My First Module

--- index.js
+++ index.js
@@ -58,34 +58,67 @@
 
 // req.end()
 
-const https = require("https")
+// const https = require("https")
 
-const data = JSON.stringify({
-  todo: "Buy the milk",
-})
+// const data = JSON.stringify({
+//   todo: "Buy the milk",
+// })
 
-const options = {
-  hostname: "whatever.com",
-  port: 443,
-  path: "/todos",
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Content-Length": data.length,
-  },
-}
+// const options = {
+//   hostname: "whatever.com",
+//   port: 443,
+//   path: "/todos",
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Content-Length": data.length,
+//   },
+// }
 
-const req = https.request(options, (res) => {
-  console.log(`statusCode: ${res.statusCode}`)
+// const req = https.request(options, (res) => {
+//   console.log(`statusCode: ${res.statusCode}`)
 
-  res.on("data", (d) => {
-    process.stdout.write(d)
-  })
-})
+//   res.on("data", (d) => {
+//     process.stdout.write(d)
+//   })
+// })
 
-req.on("error", (error) => {
-  console.error(error)
-})
+// req.on("error", (error) => {
+//   console.error(error)
+// })
 
-req.write(data)
-req.end()
+// req.write(data)
+// req.end()
+
+// import fs from "fs"
+
+// fs.mkdir("test", { recursive: true }, (err) => {
+//   if (err) {
+//     return console.error(err)
+//   }
+//   console.log("Directory created successfully!")
+// })
+
+// fs.rename("app.txt", "app.json", (err) => {
+//   if (err) {
+//     return console.error(err)
+//   }
+
+//   // done
+// })
+// import EventEmitter from "node:events"
+
+// const eventEmitter = new EventEmitter()
+
+// eventEmitter.on("start", (start, mid, end) => {
+//   console.log(`started from ${start}, stopped at ${mid} and ended up on ${end}`)
+// })
+// eventEmitter.emit("start", 1, 50, 100)
+import { createServer } from "http"
+import { myDateTime } from "./myfirstmodule.js"
+
+createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" })
+  res.write("The date and time are currently: " + myDateTime())
+  res.end()
+}).listen(8080)
