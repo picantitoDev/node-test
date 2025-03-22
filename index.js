771aa0329520e758c1e9e9adba3359578f918c3f
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-03-21 22:47:25 -0500

    Learning MVC Pattern

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express")
 const app = express()
 const productRouter = require("./routes/productRoute.js")
 const fn = require("./routes/authorRouter.js")
+const userRoute = require("./routes/userRoute.js")
 
 app.get("/", (req, res) => {
   res.send("<h1> WTF </h1>")
 })
+
+app.use("/users", userRoute)
 
 app.use("/productos", productRouter)
 
