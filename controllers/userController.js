fafec251d0f9c3d65444710296046bf99ec26d11
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-03-21 22:51:40 -0500

    Git gud

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,4 +2,16 @@ module.exports = {
   get: (req, res) => {
     res.send("User: Piero Arturo")
   },
+
+  post: (req, res) => {
+    console.log("Post Request to Users")
+  },
+
+  put: (req, res) => {
+    console.log("Put Request to Users")
+  },
+
+  delete: (req, res) => {
+    console.log("Delete Request to Users")
+  },
 }
